c9a73d75c5d109d9ab618aaeed81bdaefaee4b87
Your Name <youremail@example.com>
2025-07-31 15:14:30 +0600

    feat: update transaction routes to include transaction history and admin access with role-based token verification

--- src/modules/transaction/transaction.routes.ts
+++ src/modules/transaction/transaction.routes.ts
@@ -1,12 +1,14 @@
-import { AuthRoutes } from './../auth/auth.route';
+
 import express from 'express';
-import { topUp, withdraw, sendMoney } from './transaction.controller';
+import { topUp, withdraw, sendMoney, getMyTransactions, getAllTransactions } from './transaction.controller';
 import { verifyToken } from '../../middlewares/verifyToken';
 
 const router = express.Router();
 
-router.post('/top-up', verifyToken, topUp);
-router.post('/withdraw', verifyToken, withdraw);
-router.post('/send', verifyToken, sendMoney);
+router.post('/top-up', verifyToken(['user', 'agent', 'admin']), topUp); //wallet a money add 
+router.post('/withdraw', verifyToken(['user', 'agent', 'admin']), withdraw); //wallet theke money uthano
+router.post('/send', verifyToken(['user', 'agent', 'admin']), sendMoney); // wallet theke money send kora
+router.get('/history', verifyToken(['user', 'agent', 'admin']), getMyTransactions); // money transation history show kora 
+router.get('/admin', verifyToken(['admin']), getAllTransactions); //admin sob kisu dekha ba manage kora
 
 export const AuthTransation = router;
