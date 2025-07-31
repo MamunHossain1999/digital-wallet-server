78c149e0deb8b9b9eff38a49b38a0629d2aef499
Your Name <youremail@example.com>
2025-07-31 15:37:51 +0600

    feat: add block wallet route for admin with token verification

--- src/modules/wallet/wallet.routes.ts
+++ src/modules/wallet/wallet.routes.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import { addMoney, withdrawMoney, sendMoney, getWalletBalance, getAllWallets } from './wallet.controller';
+import { addMoney, withdrawMoney, sendMoney, getWalletBalance, getAllWallets, blockWallet,  } from './wallet.controller';
 import { verifyToken } from '../../middlewares/verifyToken';
 
 const router = express.Router();
 
 // 🔐 Routes with role-based token verification
-router.get('/me', verifyToken(), getWalletBalance);
-router.get('/all', verifyToken(['admin']), getAllWallets);
-router.post('/add-money', verifyToken(['user']), addMoney);
-router.post('/withdraw', verifyToken(['user']), withdrawMoney);
-router.post('/send-money', verifyToken(['user']), sendMoney);
-
+router.get('/me', verifyToken(), getWalletBalance); //user nijer information dekhar jnno
+router.get('/all', verifyToken(['admin']), getAllWallets); //all wallet aksathe dekhar jnno
+router.post('/add-money', verifyToken(['user']), addMoney); //money add korar jnno
+router.post('/withdraw', verifyToken(['user']), withdrawMoney); //money othanor jnno
+router.post('/send-money', verifyToken(['user']), sendMoney);  //money pathanor jnno
+router.patch('/block/:id', verifyToken(['admin']), blockWallet); //admin block korar jnno
 export const walletRoutes = router;
